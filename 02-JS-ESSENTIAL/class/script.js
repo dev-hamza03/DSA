4deedb735d3e5e754d288d6e00217c651ea6e112
Hamza <dev.hamza03@gmail.com>
2026-03-22 08:12:27 +0530

    Relation netween two integr and strings

--- 02-JS-ESSENTIAL/class/script.js
+++ 02-JS-ESSENTIAL/class/script.js
@@ -11,5 +11,26 @@ console.log(a = b)  // 30
 
 
 
+// q2. Relation netween two integr and strings and in this also understand how JS's enginie wroks
+
+// string = string = (concatenation)
+
+let s1 = "hello";
+let s2 = " world";
+
+console.log(s1 + s2);
+
+// string = int = (concatenation)
+
+let temp = 10;
+
+console.log(s1 + temp)
+
+// string = int = (arthmatic)
+
+let temp1 = 10;
+let temp2 = 20;
+
+console.log(temp1 + temp2);
 
 
