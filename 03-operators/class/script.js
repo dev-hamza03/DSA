4a8ee1effb0b339bbc6d8c99ef735cd5e7ec41b6
Hamza <dev.hamza03@gmail.com>
2026-03-23 20:36:11 +0530

    Relational Operators

--- 03-operators/class/script.js
+++ 03-operators/class/script.js
@@ -10,3 +10,30 @@ console.log(a - b); // 5
 console.log(a * b); // 50
 console.log(a / b); // 2
 console.log(a % b); // 0
+console.log(a ** b); // 100000
+
+
+
+
+
+// =====================
+// Relational Operators
+// =====================
+let x = 10;
+let y = 20;
+
+console.log("\nRelational:");
+console.log(x > y);   // false
+console.log(x < y);   // true
+console.log(x >= 10); // true
+console.log(x <= 5);  // false
+
+console.log(x == "10");  // true
+console.log(x === "10"); // false
+console.log(x != y);     // true
+console.log(x !== "10"); // true
+
+
+
+
+
