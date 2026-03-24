a20ce1906e37353632ca09572c7210b9cb5e23d8
Hamza <dev.hamza03@gmail.com>
2026-03-24 08:39:58 +0530

    different Math functions

---
+++ 04-Math-Functions/class/script.js
@@ -0,0 +1,32 @@
+
+// Math.abs. -> convert negative value in to positive
+console.log(Math.abs(-10));     // 10
+
+
+// Math.ceil -> convert the floating number to up 
+console.log(Math.ceil(10.8));   // 11
+
+
+// Math.floor -> conver the floating number to down
+console.log(Math.floor(10.2));  // 10
+
+
+// Math.round  -> convert the floating numbefr ceil if grater than .5 and if less than .5 convert into floor
+console.log(Math.round(15.4))    // 15
+console.log(Math.round(15.6))    // 16
+
+
+// Math.round -> removes the decimal part 
+console.log(Math.trunc(1.7383993));
+
+
+// Math.pow -> used the posering the number
+console.log(Math.pow(2, 5));
+
+
+// Math.max -> used for finding the max value
+console.log(Math.max(10, 20, 30, 40, 50));
+
+
+// Math.randome -> used for genrate random values
+console.log(Math.random())
